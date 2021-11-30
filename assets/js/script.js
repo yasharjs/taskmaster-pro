61f6f78dc698300983a84b592ebc07e05d2f0fbf
Yashar Sarabi <77018659+yasharjs@users.noreply.github.com>
2021-11-29 23:25:55 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -138,15 +138,24 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event){
     console.log("activate",this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event){
     console.log("deactive",this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event){
     console.log("over",event.target);
+    $(this).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event){
     console.log("out",event.target);
+    $(this).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
+
   },
   update: function(event){
     //array to store the task data
@@ -233,7 +242,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
